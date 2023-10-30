01ce3c338e99aee76c593813c46a3ee15bd82262
gbiz123 <63376055+gbiz123@users.noreply.github.com>
2023-10-30 15:11:17 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_21.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt update
-apt install nodejs -y
+apt install nodejs npm -y
 
 # Install and set up command line tools
 wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
